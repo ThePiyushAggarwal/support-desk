bd343f74e39b65facc157e07111cc3df3a8846fa
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-05-02 11:14:27 +0530

    Note display frontend

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,14 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice'
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice'
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice'
 import { useParams, useNavigate } from 'react-router-dom'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 import { toast } from 'react-toastify'
+import NoteItem from '../components/NoteItem'
 
 function Ticket() {
   const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.tickets
+  )
+
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
   )
 
   const dispatch = useDispatch()
@@ -21,6 +27,7 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId))
+    dispatch(getNotes(ticketId))
   }, [dispatch, isError, message, ticketId])
 
   // Close Ticket
@@ -30,7 +37,7 @@ function Ticket() {
     navigate('/tickets')
   }
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />
   }
 
@@ -58,7 +65,13 @@ function Ticket() {
             <h3>Description of Issue</h3>
             <p>{ticket.description}</p>
           </div>
+          <h2>Notes</h2>
         </header>
+
+        {notes.map((note) => (
+          <NoteItem key={note._id} note={note} />
+        ))}
+
         {ticket.status !== 'closed' && (
           <button onClick={onTicketClose} className="btn btn-block btn-danger">
             Close Ticket
